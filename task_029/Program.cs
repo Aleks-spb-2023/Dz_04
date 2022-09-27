2864897a17932df3415fb80d50e895fe1684a75d
Sasha <sasha@example.com>
2022-09-27 20:36:40 +0300

    массив

---
+++ task_029/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt16(Console.ReadLine());
+int [] numbers = new int [size];
+Console.Write("[");
+for(int i = 0; i < numbers.Length - 1 ; i++)
+{
+    numbers[i] = new Random().Next(10,100);
+    Console.Write(numbers[i] + " ,");
+    
+}
+ Console.Write(numbers[numbers.Length -1]);
+ Console.WriteLine("]"); 
+
+ 
